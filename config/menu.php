414a2b58a40531392d850a66b689fb11a5f8d73e
anarch02 <mrjianarchist@gmail.com>
2023-06-23 14:57:55 +0500

    dasd

--- config/menu.php
+++ config/menu.php
@@ -4,31 +4,31 @@ return[
     [
         'title' => 'Dashboard',
         'route' => 'admin.dashboard',
-        'icon' => 'bi bi-grid'        
+        'icon' => 'bi bi-grid'
     ],
     [
         'title' => 'Admin',
         'route' => 'admin.index',
-        'icon' => 'bi bi-person'        
+        'icon' => 'bi bi-person'
     ],
     [
         'title' => 'Branches',
         'route' => 'branch.index',
-        'icon' => 'bi bi-building'        
+        'icon' => 'bi bi-building'
     ],
     [
         'title' => 'Subjects',
         'route' => 'subject.index',
-        'icon' => 'bi bi-building'        
+        'icon' => 'bi bi-book-fill'
     ],
     [
         'title' => 'Groups',
         'route' => 'group.index',
-        'icon' => 'bi bi-building'        
+        'icon' => 'bi bi-microsoft-teams'
     ],
     [
         'title' => 'Students',
         'route' => 'user.index',
-        'icon' => 'bi bi-people'        
+        'icon' => 'bi bi-people'
     ],
 ];
--- resources/views/pages/action.blade.php
+++ resources/views/pages/action.blade.php
@@ -10,7 +10,14 @@
             <!-- Custom Styled Validation with Tooltips -->
             <form class="row g-3 needs-validation" action="{{isset($object)? route($info['route'], $object->id) : route($info['route'])}}" novalidate>
               @csrf
-              
+                @if(isset($object))
+                    @method('post')
+                @else
+                    @method('put')
+                @endif
+
+                @dump($info['route'])
+                {{isset($object)? route($info['route'], $object->id) : route($info['route'])}}
               @foreach ($info['selects'] as $select)
               <div>
                 <label for="{{$select['id']}}" class="form-label">{{$select['label']}}</label>
@@ -31,13 +38,13 @@
               @foreach ($info['inputs'] as $key => $item)
               <div class="position-relative">
                 <label for="validationTooltip01" class="form-label">{{$item['label']}}</label>
-                <input type="{{$item['type']}}" 
+                <input type="{{$item['type']}}"
                 class="form-control"
                 id="{{$item['name']}}"
-                name="{{$item['name']}}" 
+                name="{{$item['name']}}"
                 @isset($object)
                 value="{{$object[$item['name']]}}"
-                @endisset 
+                @endisset
                 placeholder="" required>
                 <div class="valid-tooltip">
                   Looks good!
@@ -63,12 +70,12 @@
             </h5>
           </div>
           <div class="card-body">
-            <p class="p-2"> 
+            <p class="p-2">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus rerum iure et illum iste recusandae dolore, expedita quo obcaecati rem inventore eveniet, deleniti reprehenderit fugiat dolores repudiandae alias laudantium pariatur tenetur modi. Eius quos laboriosam, placeat esse nulla explicabo voluptatum facilis laborum! Deserunt corporis, consectetur totam, tempore maiores, quibusdam doloremque natus obcaecati tenetur quaerat cumque quas iure consequuntur ad eius distinctio nemo quis. Nemo odio placeat officiis natus! Aliquam adipisci assumenda voluptates corporis cum repellat veniam, in molestiae sed amet voluptatem ipsa atque autem, obcaecati ullam ut voluptatibus id animi qui suscipit dolor. Veritatis sapiente, exercitationem minima commodi harum vitae?
             </p>
           </div>
         </div>
       </div>
     </div>
-  </section> 
+  </section>
 @endsection
